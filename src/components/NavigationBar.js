4c5477c6ff6a24be6850e02a56139628ae0b45d6
Justin Song <justin57391@gmail.com>
2024-04-30 22:34:23 -0700

    Updated projects page

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -22,30 +22,26 @@ function NavigationBar() {
         });
     }
     return (
-        <html>
-            <body>
-                <div className="navigationBar" id={expandNavigationBar ? "open" : "close"}>
-                    <div className="toggleButton">
-                        <button
-                            onClick={() => {
-                                setExpandNavigationBar((prev) => !prev);
-                            }}
-                        >
-                            <ReorderIcon />
-                        </button>
-                    </div>
-                    <div className="header">
-                        <div className="logo">justinSong();</div>
-                        <div className="links">
-                            <NavLink to="/" activeClassName="selected"> Home </NavLink>
-                            <NavLink to="/about" activeClassName="selected"> About </NavLink>
-                            <NavLink to="/projects" activeClassName="selected"> Projects </NavLink>
-                            <button className="resume" onClick={onButtonClick}>Resume</button>
-                        </div>
-                    </div>
+        <div className="navigationBar" id={expandNavigationBar ? "open" : "close"}>
+            <div className="toggleButton">
+                <button
+                    onClick={() => {
+                        setExpandNavigationBar((prev) => !prev);
+                    }}
+                >
+                    <ReorderIcon />
+                </button>
+            </div>
+            <div className="header">
+                <div className="logo">justinSong();</div>
+                <div className="links">
+                    <NavLink to="/" activeClassName="selected"> Home </NavLink>
+                    <NavLink to="/about" activeClassName="selected"> About </NavLink>
+                    <NavLink to="/projects" activeClassName="selected"> Projects </NavLink>
+                    <button className="resume" onClick={onButtonClick}>Resume</button>
                 </div>
-            </body>
-        </html>
+            </div>
+        </div>
     );
 }
 
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,5 +1,19 @@
 import React, {useState, useEffect} from 'react'
 
+function Container({name, description, languages, link, image}) {
+    return (
+        <>
+            <div class="m-[20px] w-[350px] h-[440px] md:mt-[30px] relative bg-black border-solid border-black rounded-[20px] text-center hover:shadow-s hover:-translate-y-[0.75em] hover:bg-proj duration-300 hover:border-proj">
+                    <img class="w-[325px] h-[188px] border-solid border-proj border-[1px] rounded-[10px] mt-[12.5px] mb-[12.5px] m-auto" src={image} alt={name}/>
+                    <h2 class="text-white font-montserrat text-[20px]">{name}</h2>
+                    <p class="m-[12.5px] text-gray">{description}</p>
+                    <p class="text-language text-[12px]">{languages}</p>
+                    <a class="text-white text-[12px] underline" href={link} target='_blank' rel='noreferrer'>GitHub</a>
+            </div>
+        </>
+    )
+}
+
 function Projects() {
     const [windowSize, setWindowSize] = useState(window.innerWidth);
     const handleResize = () => {
@@ -14,25 +28,25 @@ function Projects() {
     }, []);
 
     const divStyle = {
-        height: `calc(100% + ${windowSize < 768 ? '200px' : '-186px'}`
+        height: `calc(100% + ${windowSize < 768 ? '200px' : '-186px'})`
     }
 
     return (
-        <div style={divStyle} class="overflow-x-hidden md:w-full font-montserrat flex flex-col md:flex-row animate-slideInFromTop items-center md:items-flex-start">
-            <div class="m-[20px] md:m-0 w-[350px] h-[440px] md:mt-[30px] relative md:left-[150px] bg-black border-solid border-black rounded-[20px] text-center hover:shadow-s hover:-translate-y-[0.75em] hover:bg-proj duration-300 hover:border-proj">
-                <img class="w-[325px] h-[188px] border-solid border-proj border-[1px] rounded-[10px] mt-[12.5px] mb-[12.5px] m-auto" src={require("../assets/igdatascraper.png")} alt="IG Data Scraper"/>
-                <h2 class="text-white font-montserrat text-[20px]">IG Data Scraper</h2>
-                <p class="m-[12.5px] text-gray">Automated web browsing data scraper for Instagram. Pulls multiple profiles with their bio, title, follower count, individual post statistics, and more.</p>
-                <p class="text-language text-[12px]">Python, Selenium</p>
-                <a class="text-white text-[12px] underline" href='https://github.com/jjustinsong/igdatascraper' target='_blank' rel='noreferrer'>GitHub</a>
-            </div>
-            <div class="m-[20px] md:m-0 w-[350px] h-[440px] md:mt-[30px] relative md:left-[190px] bg-black border-solid border-black rounded-[20px] text-center hover:shadow-s hover:-translate-y-[0.75em] hover:bg-proj duration-300 hover:border-proj">
-                <img class="w-[325px] border-solid border-proj border-[1px] rounded-[10px] mt-[12.5px] mb-[12.5px] m-auto" src={require("../assets/flwr-man.jpg")} alt="FLWR-MAN"/>
-                <h2 class="text-white font-montserrat text-[20px]">FLWR-MAN</h2>
-                <p class="m-[12.5px] text-gray">Simple game with a 2D array-powered map and a movable/customizable character. Objective is to enter all portals and collect all the flowers.</p>
-                <p class="text-language text-[12px]">Java</p>
-                <a class="text-white text-[12px] underline" href='https://github.com/jjustinsong/flwr-man' target='_blank' rel='noreferrer'>GitHub</a>
-            </div>
+        <div style={divStyle} class="overflow-x-hidden md:w-full font-montserrat flex flex-col md:flex-row animate-slideInFromTop items-center md:items-flex-start justify-center">
+            <Container 
+                name='IG Data Scraper' 
+                description='Automated web browsing data scraper for Instagram. Pulls multiple profiles with their bio, title, follower count, individual post statistics, and more.'
+                languages='Python, Selenium'
+                link='https://github.com/jjustinsong/igdatascraper'
+                image={require('../assets/igdatascraper.png')}
+            />
+            <Container 
+                name='FLWR-MAN'
+                description='Simple game with a 2D array-powered map and a movable/customizable character. Objective is to enter all portals and collect all the flowers.'
+                languages='Java'
+                link='https://github.com/jjustinsong/flwr-man'
+                image={require('../assets/flwr-man.jpg')}
+            />
         </div>
     )
 }
